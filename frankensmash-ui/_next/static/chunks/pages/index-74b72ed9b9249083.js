198de88fad47973eff2b60b2631dd435b5f916da
lions-wrinkle <94868564+lions-wrinkle@users.noreply.github.com>
2022-12-18 19:48:17 +0100

    removed frankensmash-ui

--- frankensmash-ui/_next/static/chunks/pages/index-74b72ed9b9249083.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5356:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u(3327)}])}},function(n){n.O(0,[157,934,180,327,774,888,179],function(){return n(n.s=5356)}),_N_E=n.O()}]);
